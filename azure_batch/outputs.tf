c8ce5067999d07110829960a383dea8f4a4c6227
rchretien <renaud@radix.ai>
2022-04-01 10:09:08 +0200

    chore: added the pool_id as an output.

--- azure_batch/outputs.tf
+++ azure_batch/outputs.tf
@@ -1,3 +1,16 @@
 output "pool_id" {
   value = azurerm_batch_pool.dynamic_pool.pool_id
 }
+
+output "docker_registry" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "docker_registry_user" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "docker_registry_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
