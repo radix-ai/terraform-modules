1474393ac877d41b4a4b99c1e53776056c4bca91
rchretien <renaud@radix.ai>
2022-04-01 10:22:04 +0200

    chore: grouped providers' requirements together.

--- azure_batch/versions.tf
+++ azure_batch/versions.tf
@@ -7,3 +7,11 @@ terraform {
   }
   required_version = ">= 1.1.3"
 }
+
+# Plugin used by Terraform uses to create and manage resources.
+provider "azurerm" {
+  features {}
+
+  # Specify the subscription ID
+  subscription_id = "2f7ad274-f9f6-420a-b936-5e369c453495"
+}
